eac55941e2fb2b10352438c7e805d37981cd4218
Ifteshawn <ifteshawn@gmail.com>
2023-03-13 13:13:13 +1000

    Create custom Exception for Weather service

---
+++ Models/WeatherInformationServiceException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace WeatherApi.Models
+{
+    public class WeatherInformationServiceException : Exception
+    {
+        public HttpStatusCode StatusCode { get; }
+
+        public WeatherInformationServiceException(HttpStatusCode statusCode, string message) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
