a5bf1f35b54409aac950887fc3164499a891dbdd
Ifteshawn <ifteshawn@gmail.com>
2023-03-13 13:08:52 +1000

    Add logging

--- Authentication/ApiKeyMiddleware.cs
+++ Authentication/ApiKeyMiddleware.cs
@@ -7,11 +7,14 @@ namespace WeatherApi.Middleware
         //way to call the next thing in the middleware pipeline
         private readonly RequestDelegate _next;
         private readonly IConfiguration _configuration;
+        private readonly ILogger _logger;
 
-        public ApiKeyMiddleware(RequestDelegate next, IConfiguration configuration)
+
+        public ApiKeyMiddleware(RequestDelegate next, IConfiguration configuration, ILogger<ApiKeyMiddleware> logger)
         {
             _next = next;
             _configuration = configuration;
+            _logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext context)
@@ -25,11 +28,13 @@ namespace WeatherApi.Middleware
 
             //declaring list of API  keys from appsettings
             var apiKey = _configuration.GetSection(AuthConstants.ApiKeySectionName).Get<List<string>>();
+            _logger.LogDebug("Weather API Key values from appSettings: { apiKey }", apiKey);
 
             if (!apiKey.Contains(extractedApiKey))
             {
                 context.Response.StatusCode = 401;
                 await context.Response.WriteAsync("Invalid API Key.");
+                _logger.LogDebug("The extracted API key from request header is: {extractedApiKey}", extractedApiKey);
                 return;
             }
 
